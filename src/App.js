5abdb37a8b28f33f2f6e8a9cc307ed3562cfaf12
Brittany Blake <brittany.blake49@gmail.com>
2021-02-10 13:20:53 +0100

    add routes for pages

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Error from "./pages/Error";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
-
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
-      <Error/>
-      <SingleRoom/>
-      <Rooms/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:type" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
