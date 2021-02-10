0064aba7560a2aad31401dea4f51397e24d64ea6
Brittany Blake <brittany.blake49@gmail.com>
2021-02-10 19:55:32 +0100

    Add banner component to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
+      <Hero>
+        <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'/>
+      </Hero>
       hello from home page
     </div>
   );
