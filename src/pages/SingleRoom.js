3328621be0fe7c4ea26bd398a330b9afe3031309
Brittany Blake <brittany.blake49@gmail.com>
2021-02-10 13:51:41 +0100

    make single room into functional component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React from "react";
 import Hero from "../components/Hero";
-export class SingleRoom extends Component {
-    render() {
-        return (
-          <div>
-            <Hero hero="roomsHero" />
-            hello from single room page
-          </div>
-        );
-    }
-}
 
-export default SingleRoom
+const SingleRoom = () => {
+  return (
+    <div>
+      <Hero hero="roomsHero" />
+      hello from single room page
+    </div>
+  );
+};
+
+export default SingleRoom;
