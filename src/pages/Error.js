8a51eab4fb23c9639db4f89d68978987373e0240
Brittany Blake <brittany.blake49@gmail.com>
2021-02-11 12:18:47 +0100

    Add baanner component to error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
- const Error = () => {
-    return (
-      <div>
-        <Hero />
-        hello from error page
-      </div>
-    );
-}
+const Error = () => {
+  return (
+    <div>
+      <Hero>
+        <Banner title="404" subtitle="page not found">
+          <Link to="/" className="btn-primary">
+            {" "}
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
