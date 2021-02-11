231acbb17a78053a3e873685993098b0f65e78f4
Brittany Blake <brittany.blake49@gmail.com>
2021-02-11 12:24:51 +0100

    Add services component

---
+++ src/components/Services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const Services = () => {
+    return (
+        <div>
+            <Title title='services'></Title>
+        </div>
+    )
+}
+
+export default Services
