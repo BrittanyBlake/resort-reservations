f0c9a309e711a48b95208a31f91e8085ec7eb502
Brittany Blake <brittany.blake49@gmail.com>
2021-02-11 12:42:53 +0100

    Add content to services pagee

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,49 @@
-import React from 'react'
-import Title from './Title'
+import React, { useState } from "react";
+import Title from "./Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 
 const Services = () => {
-    return (
-        <div>
-            <Title title='services'></Title>
-        </div>
-    )
-}
+  const state = [
+    {
+      icon: <FaCocktail />,
+      title: "free cocktails",
+      info: "Ullamcorper dis velit sollicitudin. Ante augue class odio! Ac at!",
+    },
+    {
+      icon: <FaHiking />,
+      title: "Endless hiking",
+      info: "Ullamcorper dis velit sollicitudin. Ante augue class odio! Ac at!",
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: "free shuttle",
+      info: "Ullamcorper dis velit sollicitudin. Ante augue class odio! Ac at!",
+    },
+    {
+      icon: <FaBeer />,
+      title: "strongest beer",
+      info: "Ullamcorper dis velit sollicitudin. Ante augue class odio! Ac at!",
+    },
+  ];
 
-export default Services
+  const [services] = useState(state);
+
+  return (
+    <section className="services">
+      <Title title="services"></Title>
+      <div className="services-center">
+        {services.map((item, index) => {
+          return (
+            <article key={index} className="service">
+              <span>{item.icon}</span>
+              <h6>{item.title}</h6>
+              <p>{item.info}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
