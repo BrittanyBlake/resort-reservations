bb206d695d59432fe0b45f8645a4e998dab98789
Brittany Blake <brittany.blake49@gmail.com>
2021-02-11 12:16:11 +0100

    add link to rooms page to banner in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Banner from '../components/Banner'
+import Banner from "../components/Banner";
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <Hero>
-        <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'/>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+        <Link to='/rooms' className='btn-primary'> our rooms</Link>
+        </Banner>
       </Hero>
-      hello from home page
     </div>
   );
 };
