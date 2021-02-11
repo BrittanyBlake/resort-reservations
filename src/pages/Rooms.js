fa6ebe1567e44d28725a039dc11e351c54f06998
Brittany Blake <brittany.blake49@gmail.com>
2021-02-11 12:21:29 +0100

    Add banner to rooms page

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Hero from "../components/Hero";
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
  const Rooms = () => {
     return (
         <div>
-        <Hero hero='roomsHero'/>
-            hello from rooms page
+        <Hero hero='roomsHero'>
+            <Banner title='our rooms'>
+                <Link to='/' className='btn-primary'> return home</Link>
+            </Banner>
+        </Hero>
+            
         </div>
     )
 }
