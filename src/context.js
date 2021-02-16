559252c9e33a36d2563c913d4554093c567c6ca8
Brittany Blake <brittany.blake49@gmail.com>
2021-02-16 13:53:42 +0100

    add format data function to access data we want

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 import items from './data';
 
 const RoomContext = createContext()
@@ -13,6 +13,25 @@ const RoomContext = createContext()
      }
 
     const [values] = useState(initialState)
+
+    const formatData = (items)=> {
+        const tempItems = items.map( item => {
+            const id = item.sys.id;
+            const images = item.fields.images.map(image => image.fields.file.url)
+            const room = {...item.fields, images, id}
+            return room
+
+        })
+        return tempItems
+
+    }
+
+    useEffect (() => {
+        const rooms = formatData(items)
+        console.log(rooms)
+    })
+
+
     return (
       <RoomContext.Provider value={{...values}}>
         {props.children}
