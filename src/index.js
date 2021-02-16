420d33d0df662262d23dd0d541267d039ec88480
Brittany Blake <brittany.blake49@gmail.com>
2021-02-16 12:56:22 +0100

    wrap app in context provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { RoomProvider } from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
